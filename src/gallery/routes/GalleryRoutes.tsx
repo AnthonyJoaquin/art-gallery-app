09db74db10f4b7a335f8db4f62864b1d9064e8f6
202320991-user <202320991@urp.edu.pe>
2025-11-04 20:00:13 -0500

    primera entrega del ECUS 11

--- src/gallery/routes/GalleryRoutes.tsx
+++ src/gallery/routes/GalleryRoutes.tsx
@@ -1,10 +1,11 @@
 import { Navigate, Route, Routes } from 'react-router';
-import { GalleryPage } from '../pages';
+import { GalleryPage, PortfolioHealthBoard } from '../pages';
 
 export const GalleryRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<GalleryPage />} />
+  <Route path="/" element={<GalleryPage />} />
+  <Route path="/portfolio/health" element={<PortfolioHealthBoard />} />
 
       <Route path="/*" element={<Navigate to="/" />} />
     </Routes>
